47d095b44a1ddeb71200e02af76aaa89818e7202
Markus Frosch <markus.frosch@icinga.com>
2019-05-17 12:01:15 +0200

    windows: Update bootstrap script
    * Rearm Evaluation of Windows on first provision
    * Store Eval scripts
    * Restart Computer when things change

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -1,12 +1,41 @@
 # PowerShell
 
+$NeedsRestart = $FALSE
+
 Set-Culture -CultureInfo de-DE
 Set-WinUserLanguageList -LanguageList de-DE -Force
 
 Set-ExecutionPolicy Bypass -Scope Process -Force
 
-if (-not (Get-Command choco.exe -errorAction SilentlyContinue)) {
-  iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+function Test-Installed([String] $Command) {
+  if (Get-Command -Name $Command -errorAction SilentlyContinue) {
+    return $TRUE
+  } else {
+    return $FALSE
+  }
 }
 
-choco install -y --no-progress notepadplusplus
+if (-not (Test-Installed choco.exe)) {
+  iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+  $NeedsRestart = $TRUE
+}
+
+if (-not (Test-Installed notepad++.exe)) {
+  choco install -y --no-progress notepadplusplus
+}
+
+if (-not (Test-Path C:\local)) {
+  mkdir C:\local
+}
+if (-not (Test-Path C:\local\eval-rearm.ps1)) {
+  echo "Rearming evaluation for the VM..."
+  slmgr -rearm
+  $NeedsRestart = $TRUE
+
+  echo "slmgr -dlv" | Out-File C:\local\eval-status.ps1
+  echo "slmgr -rearm" | Out-File C:\local\eval-rearm.ps1
+}
+
+if ($NeedsRestart) {
+  Restart-Computer -Force
+}
