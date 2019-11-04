07745671e5b8286e1a842d01352ec880ac7396c1
Markus Frosch <markus.frosch@icinga.com>
2019-11-04 15:08:19 +0100

    Update puppet modules

--- scripts/puppet-modules.sh
+++ scripts/puppet-modules.sh
@@ -6,7 +6,20 @@ module_path=/etc/puppetlabs/code/environments/production/modules
 
 module_install() {
   if ! puppet module list | grep -q "$1"; then
-    puppet module install "$@"
+    puppet module install --ignore-dependencies --force "$@"
+  fi
+}
+
+git_module_install() {
+  local name="$1" url="$2" revision="$3"
+  local target="${module_path}/${name}"
+  if ! command -v git &>/dev/null; then
+    echo "Installing git"
+    yum install -y git
+  fi
+  if [ ! -e "$target" ]; then
+    echo "Cloning module ${name} from ${url}"
+    git clone "$url" "$target" -b "$revision"
   fi
 }
 
@@ -20,16 +33,22 @@ symlink_module() {
   fi
 }
 
-module_install puppetlabs-apache
-module_install puppetlabs-ntp
-module_install puppetlabs-mysql
-module_install puppetlabs-stdlib
-#module_install puppetlabs-vcsrepo
+# dependencies
+module_install puppetlabs-stdlib -v 6.1.0
+module_install puppetlabs-concat -v 6.1.0
+module_install puppetlabs-translate -v 2.0.0
 
-module_install lazyfrosch-vagrantenv
+module_install puppetlabs-apache -v 5.2.0
+module_install puppetlabs-ntp -v 8.1.0
+module_install puppetlabs-mysql -v 10.2.1
+module_install puppetlabs-vcsrepo -v 3.0.0
 
-module_install icinga-icinga2
-module_install icinga-icingaweb2
+module_install lazyfrosch-vagrantenv -v 0.2.1
+module_install puppet-alternatives -v 2.1.0
+
+module_install icinga-icinga2 -v 2.3.0
+#module_install icinga-icingaweb2 -v 2.3.1
+git_module_install icingaweb2 https://github.com/Icinga/puppet-icingaweb2.git bugfix/config-directory-mode
 
 symlink_module /vagrant/puppet/profile
 symlink_module /vagrant/puppet/role
