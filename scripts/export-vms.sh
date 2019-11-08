73055c42e804b36ae3af57ac64b8cf35ff9f4111
Markus Frosch <markus.frosch@netways.de>
2019-11-08 15:45:22 +0100

    scripts: Add real export

--- scripts/export-vms.sh
+++ scripts/export-vms.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 for vm in $(VBoxManage list vms | grep workshop-director | awk -F'"' '{print $2}'); do
   name="$(awk -F_ '{print $2}' <<<"$vm")"
   echo "Updating $vm -> $name"
@@ -11,3 +13,21 @@ for vm in $(VBoxManage list vms | grep workshop-director | awk -F'"' '{print $2}
   VBoxManage modifyvm "$vm" --natpf1 delete winrm-ssl &>/dev/null || true
   VBoxManage modifyvm "$vm" --name "$name"
 done
+
+if [ ! -d out ]; then
+  mkdir out
+fi
+
+export_options=( \
+  --ovf20 --manifest --options manifest
+  --vsys 0
+  --vendor "NETWAYS - Icinga Director Workshop"
+  --vendorurl "https://github.com/NETWAYS/workshop-icinga-director"
+)
+
+rm -f out/*.ova
+
+VBoxManage export master -o out/master.ova "${export_options[@]}"
+VBoxManage export satellite -o out/satellite.ova "${export_options[@]}"
+VBoxManage export agent-linux -o out/agent-linux.ova "${export_options[@]}"
+VBoxManage export agent-windows -o out/agent-windows.ova "${export_options[@]}"
